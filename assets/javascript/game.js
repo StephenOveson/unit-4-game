48cc79b254c29382598641f9b4d4bdb0130f35f9
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-02 10:09:31 -0600

    variables set ids given to coorelate with game in html on click given to crystal1

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    let userScore = 0;
+    let crystalTotal;
+    let crystal1 = 0;
+    let crystal2 = 0;
+    let crystal3 = 0;
+    let crystal4 = 0;
+
+    $('#crystal1').on('click', function(){
+        
+    });
+});
