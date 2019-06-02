7ac695f983068b8a04641c5b25869ef7d5b27230
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-02 10:59:29 -0600

    variables added and onclick is working for crystal 1 need to build match to add crystal onclick to userscore now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,27 @@
-$(document).ready(function(){
-    let userScore = 0;
-    let crystalTotal;
-    let crystal1 = 0;
-    let crystal2 = 0;
-    let crystal3 = 0;
-    let crystal4 = 0;
+$(document).ready(function (){
 
+    let userScoreDisplay = $('#user-score')
+    let userScore = 0;
+    let crystalTotal =  Math.floor(Math.random() * 101) + 19;
+    let crystal1 = Math.floor(Math.random() * 12) + 1;
+    let num1 = parseInt(crystal1);
+    let crystal2 = Math.floor(Math.random() * 12) + 1;
+    let num2 = parseInt(crystal2);
+    let crystal3 = Math.floor(Math.random() * 12) + 1;
+    let num3 = parseInt(crystal3);
+    let crystal4 = Math.floor(Math.random() * 12) + 1;
+    let num4 = parseInt(crystal4);
+    let user
+
+
+    
+    userScoreDisplay.html(userScore);
+    $('#game-total').html(crystalTotal);
+    console.log(crystalTotal)
+    
     $('#crystal1').on('click', function(){
-        
+        console.log(crystal1)
+        userScoreDisplay.html(userScoreDisplay.html() + crystal1);
     });
-});
+    
+}) ;
